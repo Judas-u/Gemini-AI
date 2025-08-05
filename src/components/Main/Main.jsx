0df83ab597e3a2b4f98ead5430966fee1726f891
Judas-u <adishwarraj9905@gmail.com>
2025-08-06 00:00:56 +0530

    .loader property

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -56,7 +56,16 @@ import { Context } from '../../context/Context';
                 </div>
                 <div className="result-data">
                     <img src={assets.gemini_icon} alt="" />
+                    { loading 
+                    ? 
+                    <div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :
                     <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                    }
                 </div>
             </div>
             }
