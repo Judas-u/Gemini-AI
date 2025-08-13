bb9b119c810d1f62b1beb3152b16942c216f332e
Judas-u <adishwarraj9905@gmail.com>
2025-08-13 23:59:38 +0530

    Errors

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -45,10 +45,12 @@ import { Context } from '../../context/Context'
                     <img src={assets.question_icon} alt="" />
                     {extended? <p>Help</p> : null} 
                 </div>
+                
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
                     {extended? <p>Activity</p> : null}  
                 </div>
+
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt="" />
                     {extended?<p>Settings</p>: null}  
